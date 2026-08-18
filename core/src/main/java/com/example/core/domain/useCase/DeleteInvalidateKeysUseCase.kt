87e7f06f814066d5e0f6d3b982d4eff49dc27054
Vusala05 <vusaleisgandarova05@gmail.com>
2026-08-18 11:34:23 +0400

    Handle invalidate Keys

--- core/src/main/java/com/example/core/domain/useCase/DeleteInvalidateKeysUseCase.kt
+++ core/src/main/java/com/example/core/domain/useCase/DeleteInvalidateKeysUseCase.kt
@@ -3,7 +3,7 @@ package com.example.core.domain.useCase
 import com.example.core.domain.feature.CacheManager
 
 class DeleteInvalidateKeysUseCase(val cacheManager: CacheManager) {
-    suspend operator fun invoke(keys : List<String>){
-        cacheManager.invalidateKeys(keys)
+    suspend operator fun invoke(keys : String){
+        cacheManager.invalidateKey(keys)
     }
 }
